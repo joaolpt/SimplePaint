8c016685597c30c39fd2e18f369820640af3ed03
João Luís <joaoluispedrosateles145@gmail.com>
2026-09-17 18:11:30 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/uemg/simplepaint/Estilo.java
@@ -0,0 +1,51 @@
+package br.edu.uemg.simplepaint;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Estilo {
+
+    public static Paint getEstilosParaLinha(){
+        Paint estiloLinha = new Paint();
+        estiloLinha.setAntiAlias(true);
+        estiloLinha.setColor(Color.MAGENTA);
+        estiloLinha.setStyle(Paint.Style.STROKE);
+        estiloLinha.setStrokeWidth(10f);
+        estiloLinha.setStrokeJoin(Paint.Join.ROUND);
+        return estiloLinha;
+    }
+
+    public static Paint getEstilosParaLinhaVerde(){
+        Paint estiloLinha = new Paint();
+        estiloLinha.setAntiAlias(true);
+        estiloLinha.setColor(Color.GREEN);
+        estiloLinha.setStyle(Paint.Style.STROKE);
+        estiloLinha.setStrokeWidth(10f);
+        estiloLinha.setStrokeJoin(Paint.Join.ROUND);
+        return estiloLinha;
+    }
+
+    public static Paint getEstilosParaLinhaAzul(){
+        Paint estiloLinha = new Paint();
+        estiloLinha.setAntiAlias(true);
+        estiloLinha.setColor(Color.BLUE);
+        estiloLinha.setStyle(Paint.Style.STROKE);
+        estiloLinha.setStrokeWidth(10f);
+        estiloLinha.setStrokeJoin(Paint.Join.ROUND);
+        return estiloLinha;
+    }
+
+    public static Paint getEstilosParaLinhaVermelha(){
+        Paint estiloLinha = new Paint();
+        estiloLinha.setAntiAlias(true);
+        estiloLinha.setColor(Color.RED);
+        estiloLinha.setStyle(Paint.Style.STROKE);
+        estiloLinha.setStrokeWidth(10f);
+        estiloLinha.setStrokeJoin(Paint.Join.ROUND);
+        return estiloLinha;
+    }
+
+
+
+
+}
